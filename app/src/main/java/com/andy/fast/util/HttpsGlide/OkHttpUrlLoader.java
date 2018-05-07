9cb72162871e8f6586e981f268b8e839c4e1d422
leeandy007 <leeandy007@gmail.com>
2018-05-07 15:44:08 +0800

    删除掉了util下面的gradle工具

---
+++ app/src/main/java/com/andy/fast/util/HttpsGlide/OkHttpUrlLoader.java
@@ -0,0 +1,75 @@
+package com.andy.fast.util.HttpsGlide;
+
+import android.content.Context;
+
+import com.bumptech.glide.load.data.DataFetcher;
+import com.bumptech.glide.load.model.GenericLoaderFactory;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.load.model.ModelLoader;
+import com.bumptech.glide.load.model.ModelLoaderFactory;
+
+import java.io.InputStream;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by leeandy007 on 2017/8/8.
+ */
+
+public class OkHttpUrlLoader implements ModelLoader<GlideUrl, InputStream> {
+
+    private final OkHttpClient client;
+
+    public OkHttpUrlLoader(OkHttpClient client) {
+        this.client = client;
+    }
+
+    @Override
+    public DataFetcher<InputStream> getResourceFetcher(GlideUrl model, int width, int height) {
+        return new OkHttpStreamFetcher(client, model);
+    }
+
+    /**
+     * The default factory for {@link com.andy.fast.util.HttpsGlide.OkHttpUrlLoader}s.
+     */
+    public static class Factory implements ModelLoaderFactory<GlideUrl, InputStream> {
+        private static volatile OkHttpClient internalClient;
+        private OkHttpClient client;
+
+        private static OkHttpClient getInternalClient() {
+            if (internalClient == null) {
+                synchronized (Factory.class) {
+                    if (internalClient == null) {
+                        internalClient = new OkHttpClient();
+                    }
+                }
+            }
+            return internalClient;
+        }
+
+        /**
+         * Constructor for a new Factory that runs requests using a static singleton client.
+         */
+        public Factory() {
+            this(getInternalClient());
+        }
+
+        /**
+         * Constructor for a new Factory that runs requests using given client.
+         */
+        public Factory(OkHttpClient client) {
+            this.client = client;
+        }
+
+        @Override
+        public ModelLoader<GlideUrl, InputStream> build(Context context, GenericLoaderFactory factories) {
+            return new com.andy.fast.util.HttpsGlide.OkHttpUrlLoader(client);
+        }
+
+        @Override
+        public void teardown() {
+            // Do nothing, this instance doesn't own the client.
+        }
+    }
+
+}
