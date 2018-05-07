ca65f6e2ff8ebb5b9cd241a18b500b82b62e90e0
leeandy007 <leeandy007@gmail.com>
2018-05-07 17:20:30 +0800

    新增widget

--- app/src/main/java/com/andy/fast/widget/MyViewPager.java
+++ app/src/main/java/com/andy/fast/widget/MyViewPager.java
@@ -3,36 +3,39 @@ package com.andy.fast.widget;
 import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
-import android.view.KeyEvent;
 import android.view.MotionEvent;
 
 public class MyViewPager extends ViewPager {
 
-	
-	public MyViewPager(Context context) {
-		super(context);
-	}
-	
-	public MyViewPager(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
+    private boolean isScroll = true;
 
-	@Override
-	public boolean dispatchKeyEvent(KeyEvent event) {
-		return false;
-	}
+    public MyViewPager(Context context) {
+        super(context);
+    }
 
-	@Override
-	public boolean onInterceptTouchEvent(MotionEvent arg0) {
-		return false;
-	}
+    public MyViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-	@Override
-	public boolean onTouchEvent(MotionEvent arg0) {
-		return false;
-	}
 
-	
-	
+    public void canScroll(boolean isScroll) {
+        this.isScroll = isScroll;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent arg0) {
+        if (isScroll)
+            return super.onTouchEvent(arg0);
+        else
+            return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent arg0) {
+        if (isScroll)
+            return super.onInterceptTouchEvent(arg0);
+        else
+            return false;
+    }
 
 }
