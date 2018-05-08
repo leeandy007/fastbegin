62c65199e0348f2f6635230ef13856cb75a321c9
leeandy007 <leeandy007@gmail.com>
2018-05-08 23:15:37 +0800

    优化了框架

--- app/src/main/java/com/andy/fast/view/IView.java
+++ app/src/main/java/com/andy/fast/view/IView.java
@@ -4,10 +4,20 @@ import android.content.Context;
 
 public interface IView {
 
+
+    /**
+     * @return 返回上下文
+     */
     Context getContext();
 
+    /**
+     * 加载等待
+     */
     void loadView();
 
+    /**
+     * @param message 显示提示信息
+     */
     void showToast(String message);
 
 }
