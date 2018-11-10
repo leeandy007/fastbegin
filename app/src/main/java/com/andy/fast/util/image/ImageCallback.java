85b68ac5055406802be6d38cbff297e0cf00a24f
leeandy007 <leeandy007@gmail.com>
2018-11-10 08:52:48 +0800

    增加了图片加载类型

--- app/src/main/java/com/andy/fast/util/image/ImageCallback.java
+++ app/src/main/java/com/andy/fast/util/image/ImageCallback.java
@@ -1,11 +1,20 @@
 package com.andy.fast.util.image;
 
 import android.content.Context;
+import android.net.Uri;
 import android.widget.ImageView;
+
+import java.io.File;
 
 public interface ImageCallback {
 
     void load(Context context, String url, ImageView imageView, int defaultImage, int errorImage);
+
+    void load(Context context, int resId, ImageView imageView, int defaultImage, int errorImage);
+
+    void load(Context context, File file, ImageView imageView, int defaultImage, int errorImage);
+
+    void load(Context context, Uri uri, ImageView imageView, int defaultImage, int errorImage);
 
     void pause(Context context);
 
