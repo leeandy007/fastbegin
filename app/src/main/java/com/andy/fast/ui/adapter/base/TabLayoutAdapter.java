431145751c03ad82faa444a5a77a7a0917e17368
leeandy007 <leeandy007@gmail.com>
2019-10-18 11:25:44 +0800

    修复bug

--- app/src/main/java/com/andy/fast/ui/adapter/base/TabLayoutAdapter.java
+++ app/src/main/java/com/andy/fast/ui/adapter/base/TabLayoutAdapter.java
@@ -10,11 +10,11 @@ import java.util.List;
  * Created by leeandy007 on 2017/9/5.
  */
 
-public class TabFragmentAdapter extends FragmentAdapter {
+public class TabLayoutAdapter extends FragmentAdapter {
 
     private List<String> titles;
 
-    public TabFragmentAdapter(FragmentManager fm, Context context, List<Fragment> list, List<String> titles) {
+    public TabLayoutAdapter(FragmentManager fm, Context context, List<Fragment> list, List<String> titles) {
         super(fm, context, list);
         this.titles = titles;
     }
