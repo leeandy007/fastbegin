fa9ded571b832d1078454b242498a480572a6e79
leeandy007 <46726966@qq.com>
2021-04-26 13:29:22 +0800

    新增网络监听类

--- app/src/main/java/com/andy/fast/util/net/RxRest/RxRestService.java
+++ app/src/main/java/com/andy/fast/util/net/RxRest/RxRestService.java
@@ -12,6 +12,7 @@ import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.HeaderMap;
+import retrofit2.http.Headers;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
@@ -23,35 +24,44 @@ import retrofit2.http.Url;
 
 public interface RxRestService {
 
+    @Headers({"UrlName:BaseUrl"})
     @GET
     Observable<String> get(@Url String url, @HeaderMap Map<String, String> headers, @QueryMap Map<String, Object> params);
 
+    @Headers({"UrlName:BaseUrl"})
     @FormUrlEncoded
     @POST
     Observable<String> post(@Url String url, @HeaderMap Map<String, String> headers, @FieldMap Map<String, Object> params);
 
+    @Headers({"UrlName:BaseUrl"})
     @FormUrlEncoded
     @PUT
     Observable<String> put(@Url String url, @HeaderMap Map<String, String> headers, @FieldMap Map<String, Object> params);
 
+    @Headers({"UrlName:BaseUrl"})
     @DELETE
     Observable<String> delete(@Url String url, @HeaderMap Map<String, String> headers, @QueryMap Map<String, Object> params);
 
+    @Headers({"UrlName:BaseUrl"})
     @Streaming
     @GET
     Observable<ResponseBody> download(@Url String url, @HeaderMap Map<String, String> headers);
 
+    @Headers({"UrlName:BaseUrl"})
     @Multipart
     @POST
     Observable<String> upload(@Url String url, @HeaderMap Map<String, String> headers, @Part MultipartBody.Part file);
 
+    @Headers({"UrlName:BaseUrl"})
     @Multipart
     @POST
     Observable<String> uploadMore(@Url String url, @HeaderMap Map<String, String> headers, @PartMap Map<String, RequestBody> params);
 
+    @Headers({"UrlName:BaseUrl"})
     @POST
     Observable<String> postRaw(@Url String url, @HeaderMap Map<String, String> headers, @Body RequestBody body);
 
+    @Headers({"UrlName:BaseUrl"})
     @PUT
     Observable<String> putRaw(@Url String url, @HeaderMap Map<String, String> headers, @Body RequestBody body);
 }
