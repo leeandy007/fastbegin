9a78d45c4faa747edb5c21f5a85beb7617c76bd3
leeandy007 <46726966@qq.com>
2022-01-05 15:13:27 +0800

    获取mainfast中的标签value

--- app/src/main/java/com/andy/fast/util/BuildUtil.java
+++ app/src/main/java/com/andy/fast/util/BuildUtil.java
@@ -1,10 +1,13 @@
 package com.andy.fast.util;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.view.WindowManager;
 
@@ -253,7 +256,30 @@ public class BuildUtil {
 		}
 		return mPhoneNumber;
     }
-	
-	
+
+	/**
+	 * 获取application中指定的meta-data
+	 * @return 如果没有获取成功(没有对应值，或者异常)，则返回值为空
+	 */
+	public static String getAppMetaData(Context context, String key) {
+		if (context == null || TextUtils.isEmpty(key)) {
+			return null;
+		}
+		String resultData = null;
+		try {
+			PackageManager packageManager = context.getPackageManager();
+			if (packageManager != null) {
+				ApplicationInfo applicationInfo = packageManager.getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
+				if (applicationInfo != null) {
+					if (applicationInfo.metaData != null) {
+						resultData = applicationInfo.metaData.getString(key);
+					}
+				}
+			}
+		} catch (PackageManager.NameNotFoundException e) {
+			e.printStackTrace();
+		}
+		return resultData;
+	}
 	
 }
